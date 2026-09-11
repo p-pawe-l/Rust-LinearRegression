a9bd1bd609c036f6ee58ce52b3f56f129aad6666
Pablo <pawelkozikowski58@gmail.com>
2026-09-11 20:37:59 +0200

    API for csv manager

--- src/csv_manager.rs
+++ src/csv_manager.rs
@@ -1,16 +1,29 @@
 use std::collections::HashMap;
 
+pub enum Value {
+    Int(i64),
+    Float(f64),
+    Text(String),
+    Bool(bool)
+}
 pub enum Column {
     Int(Vec<i64>),
     Float(Vec<f64>),
     Text(Vec<String>),
     Bool(Vec<bool>),
 }
-pub struct CsvData { columns: HashMap<String, Column>, }
-impl CsvData {
+pub struct CsvFrame { columns: HashMap<String, Column>, }
+impl CsvFrame {
     fn get_column(&self, key: &str) -> Result<&Column, String> {
         return self.columns.get(key).ok_or(format!("Column '{}' not found.", key))
     }
+    pub fn column_i64(&self, key: &str) {}
+    pub fn column_f64(&self, key: &str) {}
+    pub fn column_string(&self, key: &str) {}
+    pub fn column_bool(&self, key: &str) {}
+    pub fn row(&self, index: i32) -> Result<HashMap<String, Value>, String> {}
 }
+
+pub fn read(filename: &str) -> CsvFrame {}
 
 
