043720329b27a38c139c7fb52e73dc48fa5b1c9d
Pawel <pawelkozikowski58@gmail.com>
2026-09-12 22:48:36 +0200

    Updated linear regression file

--- src/linear_regression.rs
+++ src/linear_regression.rs
@@ -44,50 +44,56 @@ impl DataSet {
         if dataset.is_empty() {
             return Err::<Self, String>(String::new());
         }
-        return Ok(Self {
+        Ok(Self {
             data_points: dataset,
-        });
+        })
     }
     fn get_all_x(&self) -> Vec<f64> {
         let mut x_vec: Vec<f64> = Vec::new();
         for dp in &self.data_points {
             x_vec.push(dp.get_x());
         }
-        return x_vec;
+        x_vec
     }
     fn get_all_y(&self) -> Vec<f64> {
         let mut y_vec: Vec<f64> = Vec::new();
         for dp in &self.data_points {
             y_vec.push(dp.get_y());
         }
-        return y_vec;
+        y_vec
     }
     pub fn calc_mean(&self) -> Vector2D {
         let mut mean_vec: Vector2D = Vector2D::new(0.0, 0.0);
         mean_vec.x = self.get_all_x().iter().sum::<f64>() / (self.data_points.len() as f64);
         mean_vec.y = self.get_all_y().iter().sum::<f64>() / (self.data_points.len() as f64);
-        return mean_vec;
+        mean_vec
     }
-    pub fn process<F>(&mut self, mut func: F) where F: FnMut() {
-        self.data_points.iter().for_each(func);
+    pub fn transform<F>(&mut self, f: F)
+    where
+        F: FnMut(&mut Vector2D),
+    {
+        self.data_points.iter_mut().for_each(f);
     }
+}
 
 #[allow(dead_code)]
+#[derive(Debug, Clone, Copy)]
 pub struct LinearRegressionModel {
     coefficients: Vector2D,
 }
+
 #[allow(dead_code)]
 impl LinearRegressionModel {
     pub fn new() -> Self {
-        return Self {
+        Self {
             coefficients: Vector2D::new(0.0, 0.0),
-        };
+        }
     }
     pub fn get_a(&self) -> f64 {
-        return self.coefficients.get_x();
+        self.coefficients.get_x()
     }
     pub fn get_b(&self) -> f64 {
-        return self.coefficients.get_y();
+        self.coefficients.get_y()
     }
     pub fn fit(&mut self, dataset: &DataSet) {
         let mean_vec = dataset.calc_mean();
@@ -96,14 +102,14 @@ impl LinearRegressionModel {
         let mut x_squared: f64 = 0.0;
 
         for dp in &dataset.data_points {
-            yx = yx + (dp.get_y() - mean_vec.get_y()) * (dp.get_x() - mean_vec.get_x());
-            x_squared = x_squared + (dp.get_x() - mean_vec.get_x());
+            yx += (dp.get_y() - mean_vec.get_y()) * (dp.get_x() - mean_vec.get_x());
+            x_squared += dp.get_x() - mean_vec.get_x();
         }
 
-        self.coefficients.y = yx as f64 / x_squared as f64;
+        self.coefficients.y = yx / x_squared;
         self.coefficients.x = mean_vec.get_y() - self.coefficients.y * mean_vec.get_x();
     }
     pub fn predict(&self, x: f64) -> f64 {
-        return self.get_a() * x + self.get_b();
+        self.get_a() * x + self.get_b()
     }
 }
