85081fc197dfb9110d779ce8b3efba1b9371a785
Pawel <pawelkozikowski58@gmail.com>
2026-09-12 23:05:23 +0200

    Added main and tests

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,25 @@
-mod linear_regression;
 mod csv_manager;
+mod linear_regression;
 
-fn main() {
-    println!("Hello world!");
+fn main() -> Result<(), csv_manager::CsvError> {
+    let data = csv_manager::read_csv("data/points.csv")?;
+    let x_col = data.column_f64("x")?;
+    let y_col = data.column_f64("y")?;
+
+    let mut vector2d_vec: Vec<linear_regression::Vector2D> = Vec::new();
+    assert_eq!(x_col.len(), y_col.len());
+    for i in 0..x_col.len() {
+        let new_vector: linear_regression::Vector2D =
+            linear_regression::Vector2D::new(x_col[i], y_col[i]);
+        vector2d_vec.push(new_vector);
+    }
+
+    let dataset = linear_regression::DataSet::new(vector2d_vec);
+    let mut model: linear_regression::LinearRegressionModel =
+        linear_regression::LinearRegressionModel::new();
+    model.fit(&dataset.unwrap());
+
+    println!("a: {}, b: {}", model.get_a(), model.get_b());
+
+    Ok(())
 }
-
-
