c40e8df9a85381db23aa843b063472b822682019
Pawel <pawelkozikowski58@gmail.com>
2026-09-12 22:18:23 +0200

    Updated linear regression file

--- src/linear_regression.rs
+++ src/linear_regression.rs
@@ -1,21 +1,59 @@
 #[allow(dead_code)]
-pub struct Vector2D { x: f64, y: f64 }
-#[allow(dead_code)]
-impl Vector2D {
-    pub fn new(px: f64, py: f64) -> Self { Self { x: px, y: py} }
-    pub fn get_x(&self) -> f64 { return self.x; }
-    pub fn get_y(&self) -> f64 { return self.y; }
+#[derive(Debug, Clone, Copy)]
+pub struct Vector2D {
+    x: f64,
+    y: f64,
 }
 
 #[allow(dead_code)]
-pub struct DataSet { data_points: Vec<Vector2D> }
+impl Vector2D {
+    pub fn new(px: f64, py: f64) -> Self {
+        Self { x: px, y: py }
+    }
+    pub fn get_x(&self) -> f64 {
+        self.x
+    }
+    pub fn get_y(&self) -> f64 {
+        self.y
+    }
+    pub fn scale(&mut self, k: &f64) {
+        self.x *= k;
+        self.y *= k;
+    }
+    pub fn add(&mut self, v: &Vector2D) {
+        self.x += v.get_x();
+        self.y += v.get_y();
+    }
+    pub fn add_k(&mut self, k: &f64) {
+        self.x *= k;
+        self.y *= k;
+    }
+    pub fn dot(&self, v: &Vector2D) -> f64 {
+        self.x * v.get_x() + self.y * v.get_y()
+    }
+}
+
+#[allow(dead_code)]
+pub struct DataSet {
+    data_points: Vec<Vector2D>,
+}
+
+#[allow(dead_code)]
+pub fn process_dataset(dataset: &mut DataSet, func: &dyn Fn(&mut Vector2D)) {
+    for vec in &mut dataset.data_points {
+        func(vec);
+    }
+}
+
 #[allow(dead_code)]
 impl DataSet {
-    pub fn new(dataset: Vec<Vector2D>) -> Result<Self, String> { 
-        if dataset.len() == 0 {
-            return Err::<Self, String>(format!("Dataset cannot be empty."));
+    pub fn new(dataset: Vec<Vector2D>) -> Result<Self, String> {
+        if dataset.is_empty() {
+            return Err::<Self, String>(String::new());
         }
-        return Ok(Self {data_points: dataset});
+        return Ok(Self {
+            data_points: dataset,
+        });
     }
     fn get_all_x(&self) -> Vec<f64> {
         let mut x_vec: Vec<f64> = Vec::new();
@@ -40,18 +78,28 @@ impl DataSet {
 }
 
 #[allow(dead_code)]
-pub struct LinearRegressionModel { coefficients: Vector2D }
+pub struct LinearRegressionModel {
+    coefficients: Vector2D,
+}
 #[allow(dead_code)]
 impl LinearRegressionModel {
-    pub fn new() -> Self { return Self {coefficients: Vector2D::new(0.0, 0.0)} }
-    pub fn get_a(&self) -> f64 { return self.coefficients.get_x(); }
-    pub fn get_b(&self) -> f64 { return self.coefficients.get_y();}
+    pub fn new() -> Self {
+        return Self {
+            coefficients: Vector2D::new(0.0, 0.0),
+        };
+    }
+    pub fn get_a(&self) -> f64 {
+        return self.coefficients.get_x();
+    }
+    pub fn get_b(&self) -> f64 {
+        return self.coefficients.get_y();
+    }
     pub fn fit(&mut self, dataset: &DataSet) {
         let mean_vec = dataset.calc_mean();
-        
+
         let mut yx: f64 = 0.0;
         let mut x_squared: f64 = 0.0;
-    
+
         for dp in &dataset.data_points {
             yx = yx + (dp.get_y() - mean_vec.get_y()) * (dp.get_x() - mean_vec.get_x());
             x_squared = x_squared + (dp.get_x() - mean_vec.get_x());
@@ -60,5 +108,7 @@ impl LinearRegressionModel {
         self.coefficients.y = yx as f64 / x_squared as f64;
         self.coefficients.x = mean_vec.get_y() - self.coefficients.y * mean_vec.get_x();
     }
-    pub fn predict(&self, x: f64) -> f64 { return self.get_a() * x + self.get_b(); }
+    pub fn predict(&self, x: f64) -> f64 {
+        return self.get_a() * x + self.get_b();
+    }
 }
